2bea593a3493998b1489b3b39be526dea6b819af
scripka <olga.morgan17@gmail.com>
2022-07-16 15:45:28 -0600

    Add main models for data

---
+++ src/models/CastModel.ts
@@ -0,0 +1,9 @@
+export interface CastModel {
+  name: string;
+  characterName: string;
+}
+//EXAMPLE
+//{
+//  "name": "Robin Wright",
+//  "characterName": "Edee"
+//}
---
+++ src/models/MiniMovieModel.ts
@@ -0,0 +1,15 @@
+export interface MiniMovieModel {
+  id: string;
+  title: string;
+  genres: string[]
+}
+
+//Example
+//{
+//  "id": "SP015622350000",
+//  "title": "The Marksman",
+//  "genres": [
+//      "Action",
+//      "Thriller"
+//  ]
+//}
