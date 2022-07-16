08b1affcbc52f3dd7538eaf254ca215eac3fd9d6
scripka <olga.morgan17@gmail.com>
2022-07-16 17:30:22 -0600

    Add loading state and element

--- src/features/App/App.tsx
+++ src/features/App/App.tsx
@@ -2,25 +2,35 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getAllMovieData } from '../../api/apiCalls';
 import { MiniMovieModel } from '../../models/MiniMovieModel';
+import HomePage from '../HomePage/HomePage';
+import LoadingElement from '../common/Loading';
 
-const App = () => {
+const App: React.FC = () => {
 //Todo: update states use
 const [movies, setMovies] = useState<MiniMovieModel[]>([])
 const [errors, setErrors] = useState<string>("")
-
+const [isLoading, setIsLoading] = useState<boolean>(false)
 //Todo: to test api call. Will be updated with new pages
 useEffect(() => {
+  setIsLoading(true)
   getAllMovieData().then((data) => {
+    setIsLoading(false)
     if(data.message === "Success"){
       setMovies(data?.data)
     }
   })
   .catch((error: Error) => {
+    setIsLoading(false)
     setErrors(error.message)});
 },[getAllMovieData])
 
   return (
     <div className="App">
+      {isLoading && 
+        <LoadingElement/>
+      }
+      {errors && <div>Errors!</div>}
+      {/* {!!movies.length && <HomePage moviesData={movies}/>} */}
     </div>
   );
 }
---
+++ src/features/common/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Loading.css';
+ 
+const LoadingElement:React.FC = () => {
+  return (
+    <div className="loading-element">
+      <div className="loadingio-spinner-eclipse-mja2fu4o63">
+      <div className="ldio-zeuadzs9o1"><div></div></div></div>
+    </div>
+  )
+}
+
+export default LoadingElement;
