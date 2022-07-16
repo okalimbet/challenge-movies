6e59b11484d008c57093f1f39bf3fcb94b9f8f68
scripka <olga.morgan17@gmail.com>
2022-07-16 15:46:28 -0600

    Add test api call to receive data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getAllMovieData } from './apiCalls';
+import { MiniMovieModel } from './models/MiniMovieModel';
 
-function App() {
+const App = () => {
+//Todo: update states use
+const [movies, setMovies] = useState<MiniMovieModel[]>([])
+const [errors, setErrors] = useState<string>("")
+
+//Todo: to test api call. Will be updated with new pages
+useEffect(() => {
+  getAllMovieData().then((data) => {
+    if(data.message === "Success"){
+      setMovies(data?.data)
+    }
+  })
+  .catch((error: Error) => {
+    setErrors(error.message)});
+},[getAllMovieData])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
