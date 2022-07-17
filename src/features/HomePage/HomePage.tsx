963b593a6a18d661e6d9cbd4839eea247305ccd1
scripka <olga.morgan17@gmail.com>
2022-07-16 20:54:22 -0600

    Create homepage with list of movie cards

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import { MiniMovieModel } from "../../models/MiniMovieModel"
+import MovieCard from "./MovieCard"
+import './HomePage.css';
 
 interface HomePageProps {
   moviesData: MiniMovieModel[]
 }
 const HomePage: React.FC<HomePageProps> = ({moviesData}) => {
 
-  return<>HomePage</>
+  return(
+    <section className="home-page">
+      {!moviesData?.length && <div>No data</div>}  
+      {moviesData?.length && moviesData.map((singleMovie) => {
+        return <MovieCard key={singleMovie.id} movie={singleMovie}/>}
+      )}
+    </section>)
 }
 
 export default HomePage;
