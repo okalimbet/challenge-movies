1f9c05f5c56c2dde4a19fd25af68f2f3761f5c4c
scripka <olga.morgan17@gmail.com>
2022-07-18 18:11:33 -0600

    Fix style for movie details

--- src/features/MovieDetailsView/MovieDetailsView.tsx
+++ src/features/MovieDetailsView/MovieDetailsView.tsx
@@ -28,36 +28,39 @@ const MovieDetailsView:React.FC = () => {
       event.stopPropagation();
       window.location.pathname = `/movies`;
   }
-  return <div className="movie-details-wrapper">
-    <div className="movie-details" style={{display: "flex", flexDirection: "column"}}>
-    <button className="close-button" onClick={(e) => closePage(e)} type="button" aria-label="Close">
-       <span aria-hidden="true">×</span>
-    </button>
-      <span className="movie-title" style={{fontSize: "28px"}}>{movie?.title || "Unknown"}</span>
-        <div className="movie-info-group" style={{display: "flex", width: "100%"}}>
-          <div className="left-details">
-            <span className="movie-release-date">{movie?.releaseYear || "Year is unknown"}
-            </span>
-            <div className="cast">
+  return (
+    <div className="movie-details-wrapper">
+      <div className="movie-details" style={{display: "flex", flexDirection: "column"}}>
+        <button className="close-button" onClick={(e) => closePage(e)} type="button" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+        <div className="header-group">
+          <span className="movie-title">{`${movie?.title || "Unknown"}`}</span>
+          <span className="movie-title movie-release-date">{movie?.releaseYear || "Year is unknown"}</span>
+        </div>
+        
+        <div className="cast">
+        <span className="subtitle" style={{margin: "0.2em"}}>Cast: </span>
             {
               movie?.topCast.length && (
                 movie?.topCast.map((cast) => {
                   const characterName = cast?.characterName || "Uknown";
                   const castName = cast?.name || "Uknown";
 
-                  return <span key={castName} style={{margin: "0.2em"}}>{characterName + " - " + castName}</span>
+                  return <span className="names" key={castName} style={{margin: "0.2em"}}>{characterName + " - " + castName}</span>
                 })
               )
             }
-              </div>
-          </div>
-          <div className="right-details">
+        </div>
+        <div className="movie-info-group" style={{display: "flex", width: "100%"}}>
+          <div className="movie-body">
+          <span className="subtitle">Description: </span>
             <span className="movie-description">{movie?.description || "No description provided."}
             </span>
           </div>
         </div>
       </div>
-    </div>
+    </div>)
 }
 
 export default MovieDetailsView;
